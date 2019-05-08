f72d0fd9497c6bd2dc528a5c8c6e47b4d7f3b62a
Asger Bo Lassen <asge4900@edu.campusvejle.dk>
2019-05-08 11:30:51 +0200

    Added a overview  of all renters

---
+++ ItRental.Dal/RenterRepository.cs
@@ -0,0 +1,53 @@
+﻿using ItRental.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace ItRental.Dal
+{
+    public class RenterRepository : BaseRepository
+    {
+        /// <summary>
+        /// Retrieves all equipments from the database
+        /// </summary>
+        /// <returns></returns>
+        public List<Renter> GetRenters()
+        {
+            string sql = "SELECT * FROM dbo.Renters r";
+            return HandleData(ExecuteQuery(sql));
+        }
+
+        /// <summary>
+        /// Helper method used to convert DataTable to a list of Equipments.
+        /// Returns an empty list if the parameter is null.
+        /// </summary>
+        /// <param name="dataTable"></param>
+        /// <returns></returns>
+        private List<Renter> HandleData(DataTable dataTable)
+        {
+            List<Renter> renters = new List<Renter>();
+            if (dataTable is null)
+                return renters;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Renter renter = new Renter()
+                {
+                    Id = (int)row["RenterId"],
+                    Name = (string)row["Name"],                    
+                    RenterLevel = (RenterLevel)row["RenterLevel"]
+                };
+                renters.Add(renter);
+            }
+            return renters;
+        }
+
+        //public int AddEquipment(Equipment equipment)
+        //{
+        //    string sql = $"INSERT INTO dbo.Equipments VALUES ('{equipment.Name}', '{equipment.Category}', {equipment.Units})";
+
+        //    return ExecuteNonQuery(sql);
+        //}
+    }
+}
---
+++ ItRental.Web/Pages/Renters.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ItRental.Dal;
+using ItRental.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ItRental.Web.Pages
+{
+    public class RentersModel : PageModel
+    {
+        [BindProperty]
+        public Renter Renter { get; set; }
+        public List<Renter> Renters { get; set; } = new List<Renter>();
+
+        public RentersModel()
+        {
+            RenterRepository renterRepository = new RenterRepository();
+            Renters = renterRepository.GetRenters();            
+        }
+        public void OnGet()
+        {
+
+        }
+
+        //public void OnPost()
+        //{
+        //    EquipmentRepository equipmentRepository = new EquipmentRepository();
+
+        //    equipmentRepository.AddEquipment(Equipment);
+        //}
+    }
+}
